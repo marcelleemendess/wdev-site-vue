a8d26ca5f04932ce2aa1151d35bd8ab56f1238fc
Marcelle Mendes <62451375+marcelleemendess@users.noreply.github.com>
2022-03-28 20:04:45 -0600

    responsive website w/ vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <HeaderComponent/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './styles/global.css'
+import HeaderComponent from './components/HeaderComponent';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,144 @@
+<template>
+    <header>
+        <nav class="container">
+            <a href="/"><img alt="WDEV" id="logo" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"></a>
+            
+            <img v-on:click="openMenu" alt="Abrir menu" id="menu-button" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg">
+        
+            <div v-on:click="closeMenu" id="menu-overlay" v-if="menuActive"></div>
+
+            <div id="menu-items" :class="{active: menuActive}">
+                <img alt="WDEV" id="menu-logo" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg">
+            
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/videos">Vídeos</a></li>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    data(){
+        return {
+            menuActive: false
+        }
+    },
+    methods: {
+        openMenu: function(){
+            this.menuActive = true;
+        },
+        closeMenu: function(){
+            this.menuActive = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        background-color: var(--color-background-nav);
+        width: 100%;
+        height: 60px;
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        height: 60px;
+        align-items: center;
+    }
+
+    #logo {
+        width: 130px;  
+    }
+
+    #menu-button {
+        height: 30px;
+    }
+
+    #menu-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 100vh;
+        background-color: #000000;
+        opacity: 0.8;
+    }
+
+    #menu-logo {
+        width: 110px;
+        margin-top: 30px;
+        margin-bottom: 10px;
+    }
+
+    #menu-items {
+        position: fixed;
+        top: 0;
+        right: 0;
+        background-color: var(--color-background-nav);
+        width: 60%;
+        height: 100vh;
+
+        display: none;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+    
+    #menu-items.active {
+        display: flex;
+    }
+
+    ul {
+        list-style: none;
+        text-align: center;
+    }
+
+    ul li {
+        margin: 20px 0px;
+    }
+
+    ul li a {
+        color: var(--color-text-light);
+    }
+
+    @media (min-width: 700px) {
+        #menu-logo,
+        #menu-overlay,
+        #menu-button {
+            display: none;
+        }
+
+        #menu-items {
+            display: flex;
+            height: 60px;
+            position: static;
+            width: auto;
+        }
+
+        ul{
+            display: flex;
+            height: 60px;
+            align-items: center;
+        }
+
+        ul li {
+            margin: 0;
+            margin-left: 20px;
+        }
+
+    }
+
+</style>
